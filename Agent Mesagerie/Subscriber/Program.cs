e7340b6a158e904310dc10a7c6ec0e8cec95d8e6
Dima <dexenftp@gmail.com>
2020-10-09 14:53:02 +0300

    Publisher/Subscriber

---
+++ Agent Mesagerie/Subscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Subscriber");
+
+            string topic;
+
+            Console.Write("Enter the topic: ");
+            topic = Console.ReadLine().ToLower();
+
+            var subscriberScoket = new SubscriberSocket(topic);
+
+            subscriberScoket.Connect(Settings.BROKER_IP, Settings.BROKER_PORT);
+
+            Console.WriteLine("Press any key to exit ...");
+
+            Console.ReadLine();
+        }
+    }
+}
